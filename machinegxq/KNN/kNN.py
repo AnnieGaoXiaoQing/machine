f9b51da6664e70d468eac63c38307c8bdfabf2ed
gxq <gxq741718618@163.com>
2018-02-04 17:03:46 +0800

    决策树

--- machinegxq/KNN/kNN.py
+++ machinegxq/KNN/kNN.py
@@ -1,6 +1,6 @@
+# -*- coding:utf-8 -*-
 from numpy import *
 import operator
-# -*- coding: utf-8 -*-
 
 def createDataSet():
     group = array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
@@ -9,7 +9,7 @@ def createDataSet():
 
 def classify0(inX, dataSet, labels, k):
     dataSetSize = dataSet.shape[0]
-    diffMat = tile(inX, (dataSetSize,1) - dataSet)
+    diffMat = tile(inX, (dataSetSize,1)) - dataSet
     sqDiffMat = diffMat**2
     sqDistances = sqDiffMat.sum(axis=1)
     distances = sqDistances **0.5
@@ -18,11 +18,12 @@ def classify0(inX, dataSet, labels, k):
     for i in range(k):
         voteIlabel = labels[sortedDistIndices[i]]
         classCount[voteIlabel] = classCount.get(voteIlabel,0) + 1
-    sortedClassCount = sorted(classCount.iteritems(),key=operator.itemgetter(1),reverse = true)
+    sortedClassCount = sorted(classCount.iteritems(),key=operator.itemgetter(1),reverse = True) #jiangxu
     return sortedClassCount[0][0]
 
 if __name__=="__main__":
    group,labels = createDataSet()
-   classify0([0,0],group,labels,3)
+   print classify0([0,0],group,labels,3)
+
 
 
